55a51260e4eadc1cf716c7fdaf03ed3542c95b2f
Sam Naish <sam.naish92@gmail.com>
2021-03-18 20:12:18 +0000

    feat: functions done, min css

--- src/App.js
+++ src/App.js
@@ -20,28 +20,53 @@ const App = () => {
             setCurrentRes(result.current);
             setCondition(result.current.condition);
             setLocationRes(result.location);
-            setForecast(result.forecast);
+            setForecast(result.forecast.forecastday);
         },
         (error) => {
             setError(error)
         })
     }, [])
+
+    const getDayOfWeek = (date) => {
+        const dayOfWeek = new Date(date).getDay();    
+        return isNaN(dayOfWeek) ? null : ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][dayOfWeek];
+    }
+
     if (error) {
         return <div>Error: {error.message}</div>;
     } else {
         return(
             <div className="app">
-                <img src={condition.icon} alt={"Icon "+condition.text}/>
                 <div className="app__current">
-                    <span>Temp: {currentRes.temp_c}</span>
-                    <span>location: {locationRes.name}</span>
+                    <img className="app__current-image" src={condition.icon} alt={"Icon "+condition.text}/>
+                    <div className="app__current-info">
+                        <span>Today</span>
+                        <span>{currentRes.temp_c}°</span>
+                        <span>
+                            <img className="app__current-location" src="https://www.clipartmax.com/png/small/151-1517460_icon-contact-flat-web-business-symbol-blue-location-pin-icon-png.png"/>
+                            <span>{locationRes.name}</span>
+                            
+                        </span>
+                    </div>
+                    <div className="app_days">
+                        {forecast.map((item, index) => (
+                            <div key={index} className="app__days-forecast">
+                                <h3>{getDayOfWeek(item.date)}</h3>
+                                <img className="app__forecast-icon" src={item.day.condition.icon}/>
+                                <div className="app__forecast-temp">
+                                    <p className="app__max-temp">{item.day.maxtemp_c}°</p>
+                                    <p className="app__min-temp">{item.day.mintemp_c}°</p>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
 
-                    {console.log("currentRes", currentRes)}
-                    {console.log("locationRes", locationRes)}
-                    {console.log("Forecast", forecast)}
-                    {console.log("condition", condition)}
+                    {/* {console.log("currentRes", currentRes)} */}
+                    {/* {console.log("locationRes", locationRes)} */}
+                    {/* {console.log("Forecast", forecast)} */}
+                    {/* {console.log("condition", condition)} */}
                 </div>
-                <Slate/>
+                {/* <Slate/> */}
             </div>
         )
       }
--- src/components/Slate/index.js
+++ src/components/Slate/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './index.css'
+
 const Slate = () => {
     return (
         <div className="app__slate">
