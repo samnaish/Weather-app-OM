0f6878d19f80f1f668ad020b39876a07d6613fb4
Sam Naish <sam.naish92@gmail.com>
2021-03-23 17:45:31 +0000

    hopefully added image to readme and fixed location icon not rendering

---
+++ README.md
@@ -0,0 +1,7 @@
+# Weather APP with React
+
+A project built in react, and bundled using parcel.
+
+## A picture of the project (should be below):
+![Image of Project](./images/icon.png)
+
--- src/components/Location/index.js
+++ src/components/Location/index.js
@@ -3,9 +3,12 @@ import React from 'react';
 import './index.css'
 
 const Location = ({name}) => {
+
+    const imageLink = "https://www.freeiconspng.com/uploads/blue-location-icon-26.png"
+
     return (
         <div className="app-container">
-            <img className="app__current-location" src="https://www.flaticon.com/svg/vstatic/svg/684/684809.svg?token=exp=1616177254~hmac=110c8d9ab5d6f3c80043a0f605c99a53"/>
+            <img className="app__current-location" src={imageLink}/>
             <p className="app__current-city">{name}</p>  
         </div>
     )
